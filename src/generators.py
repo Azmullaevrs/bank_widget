2e3bf7983f739c049ebe2bb7338c1061f3d3bb56
Rufat Azmullaev <Rufat-2022@mail.ru>
2024-06-20 20:21:17 +0400

    new commit

--- src/generators.py
+++ src/generators.py
@@ -1,9 +1,7 @@
 from typing import Dict, Generator
 
 
-def filter_by_currency(
-    transactions: list[Dict], currency: str = "USD"
-) -> Generator[Dict[str, object], None, None]:
+def filter_by_currency(transactions: list[Dict], currency: str = "USD") -> Generator[Dict[str, object], None, None]:
     """Возвращает итератор, который выдает по очереди операции в указанной валюте"""
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == currency:
